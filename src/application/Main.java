3c8ab4cd6136e4f34b560f3cd3f8e7781b2f6e83
AndrikSokol <AndrikSokol31@gmail.com>
2023-01-04 00:55:49 +0300

    Delete unusable package and rename fxml file

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {			
-			Parent root =FXMLLoader.load(getClass().getResource("Main2.fxml"));		
+			Parent root =FXMLLoader.load(getClass().getResource("Main.fxml"));		
 			Scene scene = new Scene(root,800,550);
 			scene.setRoot(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
